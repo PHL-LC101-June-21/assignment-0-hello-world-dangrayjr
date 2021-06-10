ab7d5e35dd3382b83f6a85abff861ad60c88100b
Dan Gray Jr <85451793+dangrayjr@users.noreply.github.com>
2021-06-10 02:50:12 +0000

    Added code in Line 4 and 5.

--- hello.js
+++ hello.js
@@ -1,6 +1,8 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
+  let message = "Hello World!";
+  console.log(message);
 	return "Hello!";
 }
  
